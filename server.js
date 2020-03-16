6ce4f333e600fe472ff52196449f03dbcb42198a
andresvo <andres@prograweb.cl>
2020-03-16 20:46:10 -0300

    Deploy to Heroku

--- server.js
+++ server.js
@@ -1,6 +1,7 @@
 const express = require('express');
 const mongoose = require('mongoose');
 const bodyParser = require('body-parser');
+const path = require('path');
 
 const items = require('./routes/api/items');
 
@@ -15,6 +16,15 @@ mongoose.connect(db)
 	.catch(err => console.log(err));
 
 app.use('/api/items', items);
+
+// Serve static assets if in production
+if(process.env.NODE_ENV === 'production') {
+	// set static folder
+	app.use(express.static('client/build'));
+	app.get('*', (req, res) => {
+		res.sendFile(path.resolve(__dirname, 'client', 'build', 'index.html'));
+	})
+}
 
 const port = process.env.PORT || 5000;
 
